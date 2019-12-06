ebbf8e6024016949b3bc5d0d9268923e1836ff68
Bubionbreakfast <bubionbreakfast@gmail.com>
2019-12-06 10:04:39 +0000

    fixed img errors on homepage

--- client/src/home/components/Home.js
+++ client/src/home/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => (
         <div id="home-pic-and_btn">
           <h4 className="header">Welcome to Godfreya's!</h4>
           <div id="home-images">
-            <img className="bottom" alt="interior restaurant" src="http://3.bp.blogspot.com/-zF7_8YHOOT4/VSFOSWHwukI/AAAAAAAADeE/wdbDbs6JbRg/s1600/19.jpg" />
+            {/* <img className="bottom" alt="interior restaurant" src="http://3.bp.blogspot.com/-zF7_8YHOOT4/VSFOSWHwukI/AAAAAAAADeE/wdbDbs6JbRg/s1600/19.jpg" /> */}
             <img className="top" alt="cosy seating area" src="https://www.thetimes.co.uk/imageserver/image/methode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F849c6304-a462-11e7-afbf-91c225a59917.jpg?crop=2667%2C1500%2C0%2C0&resize=1200" />
           </div>
           <Button id="home-reservation-btn"
